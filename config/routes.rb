322329419a7766e2a0a337fe2d0eab9cb0948511
jloman <jloman200@gmail.com>
2023-03-16 14:33:29 +0300

    Update routes and schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'devise/sessions',registrations: 'users/registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :foods, only: %i[create destroy]
-  resources :user_recipes, only: %i[create index new show destroy] do
+  resources :user_recipes, only: [:create, :index, :show, :destroy, :update] do
     resources :recipe_foods
   end
   resources :public_recipes, only: [:index]
