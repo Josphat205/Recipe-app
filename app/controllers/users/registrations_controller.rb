c979365645a0473f2dde55a84b9bdb115d08f03b
jloman <jloman200@gmail.com>
2023-03-16 14:34:37 +0300

    Linters config

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,9 +5,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  def new
-    super
-  end
+
 
   # POST /resource
   def create
@@ -66,8 +64,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   private
-   
+
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation,:name)
+    params.require(:user).permit(:email, :password, :password_confirmation, :name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'devise/sessions',registrations: 'users/registrations' }
+  devise_for :users, controllers: { sessions: 'devise/sessions', registrations: 'users/registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :foods, only: %i[create destroy]
-  resources :user_recipes, only: [:create, :index, :show, :destroy, :update] do
+  resources :user_recipes, only: %i[create index show destroy update] do
     resources :recipe_foods
   end
   resources :public_recipes, only: [:index]
