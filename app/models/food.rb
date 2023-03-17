914c4b7e647b50a88b60ac93902dbeb1451ff091
jloman <jloman200@gmail.com>
2023-03-17 12:18:39 +0300

    Add food validations

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,4 +3,8 @@
 class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
+
+  validates :name, presence: true
+  validates :measurement_unit, presence: true
+  validates :price, presence: true
 end
